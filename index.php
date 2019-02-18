2f2a8435c5dcda9e76f7ec6b1d636f69b6c2ed3d
Pratham <goradiapratham@gmail.com>
2019-02-19 00:07:16 +0800

    Initial version developed, pending responsive features

--- index.php
+++ index.php
@@ -2,8 +2,13 @@
 <head>
 	<!-- Twitter bootstrap v3.3.7 -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<!-- Custom Stylesheet -->
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:600,700" rel="stylesheet">
+	<!-- Google Fonts "Open Sans + Nunito" -->
+	<link href="https://fonts.googleapis.com/css?family=Nunito:700|Open+Sans:400,600,700" rel="stylesheet">
+	<!-- FontAwesome Library for icons -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
 </head>
 <body>
 	<div class = 'masthead'>
@@ -11,40 +16,101 @@
 		<ul class = 'navigate'>
 			<li><a>Contact Us</a></li>
 			<li><a>When</a></li>
-			<li><a>Who</a></li>
 			<li><a>Why</a></li>
+			<li><a>Who</a></li>
 			<li><a>What</a></li>
 		</ul>
 	</div>
 	<div class = 'panel1'>
 		<div class="panel1-header">
-			<span id="p1head">LESS IS MORE</span>
-			<span id="p1sub">We merge expertise, experience and innovation to transform the insurance industry</span>
+			<div class = 'panel1-cap'>
+				<span id="p1head">LESS IS MORE</span>
+				<span id="p1sub">We merge technology, expertise and innovation to develop products that transform the insurance industry</span>
+			</div>
 		</div>
 	</div>
 	<div class = 'what-panel'>
-		<span class = "panel-heading">What We Do</span>
+		<span class = "panela-heading">What We Do</span>
+		<div class = "hrline"></div>
 		<div class = "row">
 			<div class = "what-step col-sm-4">
 				<img src = "tech.png">
-				<div>We adopt powerful, modern and proven information technologies</div>
+				<div class = 'panela-sub'> We adopt powerful, modern and proven information technologies</div>
 			</div>
 			<div class = "what-step col-sm-4">
 				<img src = "team.png">
-				<div>Our experts in insurance, law and IT then determine how this technology can be used to improve your insurance company</div>
+				<div class = 'panela-sub'>Our experts in insurance, law and IT determine how this technology can be used to optimize business and operations at your insurance company</div>
 			</div>
 			<div class = "what-step col-sm-4">
 				<img src = "profit.png">
-				<div>We develop innovative products specific to your business needs, which measurably boost your business efficency, cost savings, and customer satisfaction. </div>
+				<div class = 'panela-sub'>We develop innovative products specific to your business needs, which measurably boost your business efficency, cost savings, and customer satisfaction. </div>
 			</div>
 		</div>
 	</div>
 	<div class = "who-panel">
-		<span class = "panel-heading">Who Are We</span>
-		<div class = "panel-sub">Our team consists of outstanding experts from Law, Insurance and Technology. They possess not just exceptional capabilities, but also unbeatable passion and belief in using modern technology to improve people's quality of life. We aspire to revolutionize today's insurance industry by making it smarter, safer and smoother for everyone involved.</div>
+		<div class = 'row'>
+			<span class = "panelb-heading">Who Are We</span>
+			<div class = "hrline" style="border-bottom: 1px solid #ad7800"></div>
+			<div class = "panelb-sub">LIM is an innovation and technology one-stop shop tailored for the insurance industry. Our team consists of outstanding experts from Law, Insurance and Technology. They possess not just exceptional capabilities, but also unbeatable passion and belief in using modern technology to improve people's quality of life. We aspire to revolutionize today's insurance industry by making it smarter, safer and smoother for everyone involved.</div>
+		</div>
 	</div>
 	<div class = "why-panel">
-		<span class = "panel-heading">Why Choose Us</span>
+		<span class = "panela-heading">Why Choose Us</span>
+		<div class = "hrline"></div>
+		<div class = 'row'>
+			<div class = "why-feat col-sm-3">
+				<span><i class="fas fa-award"></i></span>
+				<span class = "title">Quality Solutions</span>
+				<div class = "panela-sub" style = "margin-top: 10px">
+					We ensure high standards with regards to the technical quality and practicality of our products. Our products are user-friendly, effective and reliable.
+				</div>
+			</div>
+			<div class = "why-feat col-sm-3">
+				<span><i class="fas fa-hand-holding-usd"></i></span>
+				<span class = "title">Value For Money</span>
+				<div class = "panela-sub" style = "margin-top: 10px">
+					Being a company specialized in insurtech allows us to make our services and products great value for money, especially considering the savings and benefits they bring. We are able to serve you regardless of the size of your company.
+				</div>
+			</div>
+			<div class = "why-feat col-sm-3">
+				<span><i class="fas fa-users"></i></span>
+				<span class = "title">Specialized Team</span>
+				<div class = "panela-sub" style = "margin-top: 10px">
+					Our team of diverse experts are able to communicate and develop feasible, practical solutions. This saves you the time and resources needed to create applications in-house, with developers who seldom have experience in insurance operations.
+				</div>
+			</div>
+			<div class = "why-feat col-sm-3">
+				<span><i class="fas fa-pen-fancy"></i></span>
+				<span class = "title">Products For You</span>
+				<div class = "panela-sub" style = "margin-top: 10px">
+					We know exactly what each department within an insurer needs, and which technologies will serve you best. We develop products that match your operations and can be easily integrated in your workplace.
+				</div>
+			</div>
+		</div>
+		</div>
+		<div class = "when-panel">
+		<div class = 'row'>
+			<span class = "panelb-heading">When To Act</span>
+			<div class = "hrline" style="border-bottom: 1px solid #ad7800"></div>
+			<div class = "panelb-sub">The insurance industry is undergoing a revolution. Modern technology is already disrupting the traditional market in diverse ways, making substantial impact on business costs and revenue. Today's customers are more discerning and informed, seeking to pay less and get more. Join us now as pioneers in this transformation and get an edge over the competition. Gain an advantage by increasing efficiency, reducing costs, and making your customers' insurance journey effortless, from enrollment to claim.</div>
+		</div>
+		</div>
+		<div class = 'contact-panel'>
+			<span class = "panela-heading">Contact Us</span>
+			<div class = "hrline"></div>
+			<div class = "when-det row">
+				<div style = "font-size: 17px; margin-bottom: 20px">
+					<span style = "font-weight: 600;">We would be delighted to receive any queries or suggestions.</span> 
+					Reach us now to find out how we can impact your business.
+				</div>
+				<span class = "clabel">Address&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp:</span>
+				<span class = "cdeet">&nbsp&nbsp&nbsp&nbsp&nbspRm 1608, China Insurance Group Building, 141 Des Vouex Road Central, Hong Kong</span><br><br>
+				<span class = "clabel">Email&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp:</span>
+				<span class = "cdeet">&nbsp&nbsp&nbsp&nbsp&nbspinfo@lessismore.best</span><br><br>
+				<span class = "clabel">Telephone&nbsp&nbsp&nbsp&nbsp&nbsp:</span>
+				<span class = "cdeet">&nbsp&nbsp&nbsp&nbsp&nbsp+852 2868-3631</span>
+			</div>
+		</div>
 	</div>
 	<!-- JQUERY v3.3.1 -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
