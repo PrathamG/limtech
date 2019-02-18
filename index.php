1f18308e96d4c152a1d146840f63d325f0ca3bc1
Pratham <goradiapratham@gmail.com>
2019-02-18 14:04:10 +0800

    Developed partial skeleton structure

---
+++ index.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+	<!-- Twitter bootstrap v3.3.7 -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:600,700" rel="stylesheet">
+</head>
+<body>
+	<div class = 'masthead'>
+		<img class = 'logo' src = "logo.png">
+		<ul class = 'navigate'>
+			<li><a>Contact Us</a></li>
+			<li><a>When</a></li>
+			<li><a>Who</a></li>
+			<li><a>Why</a></li>
+			<li><a>What</a></li>
+		</ul>
+	</div>
+	<div class = 'panel1'>
+		<div class="panel1-header">
+			<span id="p1head">LESS IS MORE</span>
+			<span id="p1sub">We merge expertise, experience and innovation to transform the insurance industry</span>
+		</div>
+	</div>
+	<div class = 'what-panel'>
+		<span class = "panel-heading">What We Do</span>
+		<div class = "row">
+			<div class = "what-step col-sm-4">
+				<img src = "tech.png">
+				<div>We adopt powerful, modern and proven information technologies</div>
+			</div>
+			<div class = "what-step col-sm-4">
+				<img src = "team.png">
+				<div>Our experts in insurance, law and IT then determine how this technology can be used to improve your insurance company</div>
+			</div>
+			<div class = "what-step col-sm-4">
+				<img src = "profit.png">
+				<div>We develop innovative products specific to your business needs, which measurably boost your business efficency, cost savings, and customer satisfaction. </div>
+			</div>
+		</div>
+	</div>
+	<div class = "who-panel">
+		<span class = "panel-heading">Who Are We</span>
+		<div class = "panel-sub">Our team consists of outstanding experts from Law, Insurance and Technology. They possess not just exceptional capabilities, but also unbeatable passion and belief in using modern technology to improve people's quality of life. We aspire to revolutionize today's insurance industry by making it smarter, safer and smoother for everyone involved.</div>
+	</div>
+	<div class = "why-panel">
+		<span class = "panel-heading">Why Choose Us</span>
+	</div>
+	<!-- JQUERY v3.3.1 -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="script.js"></script>
+</body>
+</html>
